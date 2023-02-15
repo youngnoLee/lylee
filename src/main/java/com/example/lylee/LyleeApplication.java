d9427469e63b930365a3670c6d24b97aa337d4a2
aorlrk <aorlrk@naver.com>
2023-02-15 22:11:24 +0900

    Initial commit

---
+++ src/main/java/com/example/lylee/LyleeApplication.java
@@ -0,0 +1,13 @@
+package com.example.lylee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LyleeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LyleeApplication.class, args);
+	}
+
+}
